fd57fb3da243318c5484b28276b7e58fbd84eb7f
unknown <interviewer@devspare-laptop.7digital.local>
2015-06-26 13:34:38 +0100

    Initial Commit

---
+++ Skeleton/CheckoutKata/MusicSalesTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace MusicSalesTests
+{
+    [TestFixture]
+    public class MusicSalesTest
+    {
+        [Test]
+        public void Tests_are_running()
+        {
+            const string betterSystems = "EmergentDesign";
+            Assert.That("EmergentDesign", Is.EqualTo(betterSystems));
+        }
+    }
+}
